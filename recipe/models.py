a8035f62d343013dbfb47dffcaa0bfb44b51e1e4
olya <bachyrinaolya94@gmail.com>
2026-05-21 15:35:17 +0300

    Add Category model

--- recipe/models.py
+++ recipe/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
-class Category(models.Model):
-    
-    name = models.CharField(max_length=100)
-    
-    def __str__(self):
 
-        return self.name()
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructions = models.TextField()
+    ingredients = models.TextField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
